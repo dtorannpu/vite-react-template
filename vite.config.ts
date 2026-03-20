c017762edb58d743014d817fb1f2a16aaa3ae883
dtorannpu <dtorannpu@gmail.com>
2026-03-20 09:24:53 +0900

    Add @rolldown/plugin-babel to dependencies and update Vite config for Babel integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     tsconfigPaths: true,
   },
